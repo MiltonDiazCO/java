7f5571e218aba2172192f1ee16bd5b9e6cb54b67
MiltonDiazCO <diazmartinezmilton@gmail.com>
2021-12-30 12:51:12 +0000

    enums

---
+++ src/basico/Enums.java
@@ -0,0 +1,15 @@
+package basico;
+
+public class Enums {
+
+	// creando el ENUM con sus valores
+	enum Paises {
+		Colombia, Mexico, España
+	};
+
+	// un ENUM solo puede almacenar valores definidos en la lista
+	Paises paisUno = Paises.Colombia;
+	Paises paisDos = Paises.Mexico;
+	Paises paisTres = Paises.España;
+
+}
